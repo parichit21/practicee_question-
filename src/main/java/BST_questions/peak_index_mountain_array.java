1a04c883f5c9d224a4e370f31852fbf32b298043
parichit21 <parichitkukreti@gmail.com>
2025-03-21 23:21:40 +0530

    added new fie

--- src/main/java/BST_questions/peak_index_mountain_array.java
+++ src/main/java/BST_questions/peak_index_mountain_array.java
@@ -7,14 +7,15 @@ public class peak_index_mountain_array {
 //        Input: arr = [0,1,0]
 //        Output: 1
         int[] arr = {0,1,0};
-
+       int ans =  peakIndexInMountainArray(arr);
+       System.out.println(ans);
     }
 
-    public int peakIndexInMountainArray(int[] arr) {
+    static int peakIndexInMountainArray(int[] arr) {
 
         int start=0;
-        int end = arr.length;
-        while(start<=end)
+        int end = arr.length-1;
+        while(start<end)
         {
             int mid = start + (end-start)/2;
             if(arr[mid]>arr[mid+1])
@@ -25,15 +26,16 @@ public class peak_index_mountain_array {
                 end = mid;
             }else {
                 /*you are in asc part of array*/
-                start=mid+1;
+                start=mid+1;   //because we know that mid+1 element > mid element
             }
-
-
-
-
         }
+/*             in the end , start == end pointing to the largest number because of the 2 checks above
+*               sttart and the end are always trying to find the max element in the above 2 checks
+*               hence when they are pointing to just one element that is the max one element
+*                more elaboration : at every point for start and end, they have the best possible answer till that time
+*               and if we are saving one item is remaining hence because of the above checks    */
 
 
-         return -1;
+         return start;
     }
 }
