84c269fb7085f71774a88e5b22fd640364d05758
parichit21 <parichitkukreti@gmail.com>
2025-03-27 00:18:23 +0530

    cyclic sort practice

--- src/main/java/LTquestions_arrays/missingNumber.java
+++ src/main/java/LTquestions_arrays/missingNumber.java
@@ -15,71 +15,99 @@ public class missingNumber {
       */
     public static void main(String[] args) {
         int [] nums = {3,0,1};
-        bubblesort(nums);
+      //  bubblesort(nums);
         int ans = missingNumber(nums);
             System.out.println(ans);
     }
-    static int missingNumber(int[] nums) {
+//    static int missingNumber(int[] nums) {
+//
+//        int start =0;
+//        int last = nums.length-1;
+//        while(start<=last)
+//        {
+//            if(nums.length<2 && nums[0]==1)
+//            {
+//                return 0;
+//            }
+//            if(nums[0] !=0)
+//            {
+//                return 0;
+//            }
+//            if(nums.length<2 && nums[0]==0)
+//            {
+//                return 1;
+//            }
+//            if(nums.length == 2)
+//            {
+//                if(nums[start]+1 != nums[last])
+//                {
+//                    return nums[start]+1;
+//                }
+//              return  nums[last]+1;
+//            }
+//            if(nums[start] < nums[start+1] && nums[start]+1 == nums[start+1])
+//            {
+//                start++;
+//            }
+//            if(nums[start] > nums[start+1] && nums[start+1]-1 == nums[start])
+//            {
+//                start--;
+//            }
+//            else if(nums[start]+1 != nums[start+1])
+//            {
+//                return nums[start]+1;
+//            }
+//        }
+//    return 0;
+//    }
 
-        int start =0;
-        int last = nums.length-1;
-        while(start<=last)
+//    static void bubblesort(int [] nums){
+//        boolean swap;
+//
+//        for(int i=0;i<nums.length;i++)
+//        {
+//            swap =false;
+//            for(int j=1;j<nums.length-i;j++)
+//            {
+//                if(nums[j]<nums[j-1]){
+//                    int temp = nums[j];
+//                    nums[j] =nums[j-1];
+//                    nums[j-1] = temp;
+//                    swap =true;
+//                }
+//            }
+//            if(!swap) {break;}
+//        }
+//
+//
+//    }
+
+    static int missingNumber(int[] nums) {
+        int i =0;
+        while(i<nums.length)
         {
-            if(nums.length<2 && nums[0]==1)
+            int correct = nums[i];
+            if(nums[i]<nums.length &&  nums[i] != nums[correct])
             {
-                return 0;
-            }
-            if(nums[0] !=0)
+                swap(nums, i,correct);
+            }else{i++;}
+        }
+//            search for the missing number
+        for(int j =0; j<nums.length;j++)
+        {
+            if(nums[j] !=j)
             {
-                return 0;
-            }
-            if(nums.length<2 && nums[0]==0)
-            {
-                return 1;
-            }
-            if(nums.length == 2)
-            {
-                if(nums[start]+1 != nums[last])
-                {
-                    return nums[start]+1;
-                }
-              return  nums[last]+1;
-            }
-            if(nums[start] < nums[start+1] && nums[start]+1 == nums[start+1])
-            {
-                start++;
-            }
-            if(nums[start] > nums[start+1] && nums[start+1]-1 == nums[start])
-            {
-                start--;
-            }
-            else if(nums[start]+1 != nums[start+1])
-            {
-                return nums[start]+1;
+                return j;
             }
         }
-    return 0;
+
+        return nums.length;
     }
 
-    static void bubblesort(int [] nums){
-        boolean swap;
-
-        for(int i=0;i<nums.length;i++)
-        {
-            swap =false;
-            for(int j=1;j<nums.length-i;j++)
-            {
-                if(nums[j]<nums[j-1]){
-                    int temp = nums[j];
-                    nums[j] =nums[j-1];
-                    nums[j-1] = temp;
-                    swap =true;
-                }
-            }
-            if(!swap) {break;}
-        }
-
-
+    private static void swap(int[] arr, int first, int secord) {
+                int temp = arr[first];
+                arr[first] = arr[secord];
+                arr[secord] = temp;
     }
 
 }
